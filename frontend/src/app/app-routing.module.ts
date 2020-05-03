b1aff858910ebebb3413a5b3191b5232cd808633
VictorTI2018 <victorhugoTI@outlook.com>
2020-05-03 15:48:19 -0300

    Começando a criar rotina handleSubmit

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './views/home/home.component'
 import { AutorComponent } from './views/autor/autor.component';
 import { AutorCreateComponent } from './views/autor/autor-create/autor-create.component';
 import { EditoraComponent } from './views/editora/editora.component';
+import { EditoraCreateComponent } from './views/editora/editora-create/editora-create.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -14,7 +15,8 @@ const routes: Routes = [
   },
   { path: 'autor-create', component: AutorCreateComponent },
   { path: 'autor-edit', component: AutorCreateComponent },
-  { path: 'editoras', component: EditoraComponent }
+  { path: 'editoras', component: EditoraComponent },
+  { path: 'editora-create', component: EditoraCreateComponent }
 ];
 
 @NgModule({
--- frontend/src/app/views/editora/editora-create/editora-create.component.ts
+++ frontend/src/app/views/editora/editora-create/editora-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms'
+import { EditoraService } from 'src/app/services/editora/editora.service';
 
 @Component({
   selector: 'app-editora-create',
@@ -11,20 +12,26 @@ export class EditoraCreateComponent implements OnInit {
   editoraForm: FormGroup
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private editoraService: EditoraService
   ) { }
 
   ngOnInit(): void {
     this.editoraForm = this.formBuilder.group({
+      id: [''],
       nome: ['']
     })
   }
 
   handleSubmit() {
     if(this.editoraForm.valid) {
-
+      const model = Object.assign(this.editoraForm.value)
+      let res;
+      if(!model.id) {
+        res = this.editoraService
+      }
     } else {
-      
+
     }
   }
 
